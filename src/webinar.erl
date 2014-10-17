1e9d9f5b66d961d9ad758173aa16016568b9f310
Brujo Benavides <elbrujohalcon@gmail.com>
2014-10-17 15:30:49 -0300

    Paramterizable Invitee

--- src/webinar.erl
+++ src/webinar.erl
@@ -1,8 +1,6 @@
 -module(webinar).
 
--export([speaker/0, name/0, invitation/0]).
-
--define(speaker, speaker()).
+-export([speaker/0, name/0, invitation/0, invitation/1]).
 
 -spec speaker() -> string().
 speaker() -> "Brujo Benavides".
@@ -11,9 +9,13 @@ speaker() -> "Brujo Benavides".
 name() -> "Elvis: how to properly rock in the Erlang World!".
 
 -spec invitation() -> string().
-invitation() ->
-  "Erlang Solutions invites you to join our next webinar featuring "
-  ++ ?speaker ++ " (Inaka's CTO)."
+invitation() -> invitation(you).
+
+-spec invitation(you|string()) -> string().
+invitation(you) -> invitation("you");
+invitation(_Invitee) ->
+  "Erlang Solutions invites " ++ _Invitee ++ " to join our next webinar"
+  " featuring " ++ speaker() ++ " (Inaka's CTO)."
   " He'll present an overview of Elvis - a style-checker written in Erlang, and"
   " will let you know how it works and how it was built."
   " He will also open the discussion about Erlang Guidelines and code styles.".
